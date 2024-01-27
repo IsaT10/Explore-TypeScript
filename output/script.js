c10f38103b6cb9b6b34bd5274685982b816ff14c
IsaT <rakiib010@gmail.com>
2024-01-27 09:30:25 +0600

    explicit TypeScript syntax

--- output/script.js
+++ output/script.js
@@ -1,3 +1,32 @@
 "use strict";
-const myName = 'Ishat';
-console.log(myName);
+// function multiply(a, b) {
+//   return a * b; //NaN
+// }
+// console.log(multiply('sadsa', 9));
+// let fruits = ['apple', 'banana', 'mango'];
+// fruits.push(9)
+// explicit types
+// let myName: string = 'Ishat';
+// console.log(myName);
+// let age: number = 23;
+// console.log(age);
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+// console.log(multiply(9, 9));
+// let fruits: string[] = ['apple', 'banana', 'mango'];
+// fruits.push('orange');
+// let mixed: (string | number | boolean)[] = ['mango', 10, true];
+// mixed.push(false);
+// console.log(mixed);
+// let obj: {
+//   name: string;
+//   age: number;
+//   adult: boolean;
+// };
+// obj = {
+//   name: 'ishat',
+//   age: 23,
+//   adult: true,
+// };
+// console.log(obj);
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,43 @@
-const myName = 'Ishat';
+// function multiply(a, b) {
+//   return a * b; //NaN
+// }
 
-console.log(myName);
+// console.log(multiply('sadsa', 9));
+
+// let fruits = ['apple', 'banana', 'mango'];
+// fruits.push(9)
+
+// explicit types
+
+// let myName: string = 'Ishat';
+// console.log(myName);
+
+// let age: number = 23;
+// console.log(age);
+
+// function multiply(a: number, b: number) {
+//   return a * b;
+// }
+
+// console.log(multiply(9, 9));
+
+// let fruits: string[] = ['apple', 'banana', 'mango'];
+// fruits.push('orange');
+
+// let mixed: (string | number | boolean)[] = ['mango', 10, true];
+// mixed.push(false);
+// console.log(mixed);
+
+// let obj: {
+//   name: string;
+//   age: number;
+//   adult: boolean;
+// };
+
+// obj = {
+//   name: 'ishat',
+//   age: 23,
+//   adult: true,
+// };
+
+// console.log(obj);
